b1290f38f6a6969aa3175ba38047bf9ede14fa1a
Natasha Klingenbrunn <natashaklingenbrunn@mbptilhenatasha.clients.wireless.dtu.dk>
2020-11-09 14:48:15 +0100

    plotting in main

--- main.py
+++ main.py
@@ -4,11 +4,38 @@ import os
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sn
 
-cwd = os.getcwd()
-df = pd.read_csv(cwd+'/data/sensor1.csv')
-print(df.head())
+# Importing data
+df = pd.read_csv(os.getcwd()+'/Data/sensor1.csv')
 
-df_sensor_1 = df[df['sensor_id'] == 1]
-_ = df_sensor_1.set_index('timestamp')[['temperature', 'humidity', 'ohms', 'moisture']].plot(subplots=True, figsize=(23, 14))
+# Understanding Data
+print(df.tail())
+
+# Humidity at sensor
+# ax = df.humidity.plot.line()
+# ax = df.humidity.plot.hist(bins=100)
+
+
+# Weather Humidity
+# ax = df.weather_humidity.plot.hist(bins=100)
+
+# Wind
+# ax = df.weather_wind_dir.plot.density()
+# ax = df.weather_wind_dir.plot.hist(bins=100)
+
+# Season
+# print(df.timestamp.min())
+# print(df.timestamp.max())
+
+# Correlation Matrix
+corrMatrix = df[df['sensor_id'] == 4].corr()
+sn.heatmap(corrMatrix)
 plt.show()
+
+# Plotting
+# ax.plot()
+# ax.set_xlabel("Degrees")
+# ax.set_ylabel("Frequency")
+# ax.set_title("Wind Direction")
+# plt.show()
