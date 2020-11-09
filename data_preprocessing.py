4f0e16646d023929775ec4a0ea45e2f4fc893365
osta0005 <olinestaerke@hotmail.com>
2020-11-09 15:34:48 +0100

    Implementing LSTM in main

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 from plotting import plot_time_series_class
+import missingno as msno #Missing values plot
 
 RANDOM_SEED = 42
 
@@ -20,13 +21,28 @@ cwd = os.getcwd()
 df = pd.read_csv(cwd+'/data/sensor1.csv',infer_datetime_format=True, parse_dates={'datetime':[0,1]}, index_col=['datetime'])
 
 #Find the null values for each attribute
-print(df.isnull().sum())
+#print(df.isnull().sum())
 # This shows that we have a lot of null values. ^
+
+#### Visualizing missing value
+
+#Show missing values for all attributes -> White lines indicate a missing value. From this plot it seems that some of the attrobutes are correlated
+#according to missing values
+#msno.matrix(df)
+#plt.show()
+
+#Shows a heatmap of the attributes where the missing values are correlated
+#msno.heatmap(df)
+#plt.show()
+
+#Shows missing values as a bar plot
+#msno.bar(df)
+#plt.show()
 
 #Forward will all NaN values with previous existing values
 df.fillna(method='ffill', inplace=True)
 
-print(df.isnull().sum())
+#print(df.isnull().sum())
 #^This shows us that we have one attribute where all values are Nan. Apart from this, is looks like it worked.
 
 
@@ -42,7 +58,6 @@ for i in df.columns:
     scalers['scaler_'+ i] = scaler
     df[i]=s_s
 
-print(df.head())
 
 
     
@@ -50,11 +65,13 @@ print(df.head())
 
 #shuffle the elements
 df = df.sample(frac=1.0)
+df = df[['temperature','ohms']] 
 
+print(df.head())
 #Splitting the data in to training, validation and test
 train_df, val_df = train_test_split(
   df,
-  test_size=0.15,
+  test_size=0.80,
   random_state=RANDOM_SEED
 )
 val_df, test_df = train_test_split(
@@ -62,3 +79,6 @@ val_df, test_df = train_test_split(
   test_size=0.33,
   random_state=RANDOM_SEED
 )
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,190 @@
-import io
-import json
-import os
+import torch
 
-import matplotlib.pyplot as plt
+import copy
+import numpy as np
 import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.model_selection import train_test_split
 
-cwd = os.getcwd()
-df = pd.read_csv(cwd+'/data/sensor1.csv')
-print(df.head())
+from torch import nn, optim
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-df_sensor_1 = df[df['sensor_id'] == 1]
-_ = df_sensor_1.set_index('timestamp')[['temperature', 'humidity', 'ohms', 'moisture']].plot(subplots=True, figsize=(23, 14))
+
+import torch.nn.functional as F
+from arff2pandas import a2p
+from data_preprocessing import val_df, test_df, train_df
+
+def create_dataset(df):
+    sequences = df.astype(np.float32).to_numpy().tolist()
+    dataset = [torch.tensor(s).unsqueeze(1).float() for s in sequences]
+    n_seq, seq_len, n_features = torch.stack(dataset).shape
+    return dataset, seq_len, n_features
+
+
+    train_dataset, seq_len, n_features = create_dataset(train_df)
+
+train_dataset, seq_len, n_features = create_dataset(train_df)
+val_dataset, _, _ = create_dataset(val_df)
+test_normal_dataset, _, _ = create_dataset(test_df)
+
+class Encoder(nn.Module):
+
+  def __init__(self, seq_len, n_features, embedding_dim=64):
+    super(Encoder, self).__init__()
+
+    self.seq_len, self.n_features = seq_len, n_features
+    self.embedding_dim, self.hidden_dim = embedding_dim, 2 * embedding_dim
+
+    self.rnn1 = nn.LSTM(
+      input_size=n_features,
+      hidden_size=self.hidden_dim,
+      num_layers=1,
+      batch_first=True
+    )
+    
+    self.rnn2 = nn.LSTM(
+      input_size=self.hidden_dim,
+      hidden_size=embedding_dim,
+      num_layers=1,
+      batch_first=True
+    )
+
+  def forward(self, x):
+    x = x.reshape((1, self.seq_len, self.n_features))
+
+    x, (_, _) = self.rnn1(x)
+    x, (hidden_n, _) = self.rnn2(x)
+
+    return hidden_n.reshape((self.n_features, self.embedding_dim))
+
+
+class Decoder(nn.Module):
+
+  def __init__(self, seq_len, input_dim=64, n_features=1):
+    super(Decoder, self).__init__()
+
+    self.seq_len, self.input_dim = seq_len, input_dim
+    self.hidden_dim, self.n_features = 2 * input_dim, n_features
+
+    self.rnn1 = nn.LSTM(
+      input_size=input_dim,
+      hidden_size=input_dim,
+      num_layers=1,
+      batch_first=True
+    )
+
+    self.rnn2 = nn.LSTM(
+      input_size=input_dim,
+      hidden_size=self.hidden_dim,
+      num_layers=1,
+      batch_first=True
+    )
+
+    self.output_layer = nn.Linear(self.hidden_dim, n_features)
+
+  def forward(self, x):
+    x = x.repeat(self.seq_len, self.n_features)
+    x = x.reshape((self.n_features, self.seq_len, self.input_dim))
+
+    x, (hidden_n, cell_n) = self.rnn1(x)
+    x, (hidden_n, cell_n) = self.rnn2(x)
+    x = x.reshape((self.seq_len, self.hidden_dim))
+
+    return self.output_layer(x)
+
+
+
+class RecurrentAutoencoder(nn.Module):
+
+  def __init__(self, seq_len, n_features, embedding_dim=64):
+    super(RecurrentAutoencoder, self).__init__()
+
+    self.encoder = Encoder(seq_len, n_features, embedding_dim).to(device)
+    self.decoder = Decoder(seq_len, embedding_dim, n_features).to(device)
+
+  def forward(self, x):
+    x = self.encoder(x)
+    x = self.decoder(x)
+
+    return x
+
+print("Create model")
+model = RecurrentAutoencoder(seq_len, n_features, 128)
+model = model.to(device)
+
+
+def train_model(model, train_dataset, val_dataset, n_epochs):
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+  criterion = nn.L1Loss(reduction='sum').to(device)
+  history = dict(train=[], val=[])
+
+  best_model_wts = copy.deepcopy(model.state_dict())
+  best_loss = 10000.0
+  
+  for epoch in range(1, n_epochs + 1):
+    print("epoch", epoch)
+    model = model.train()
+
+
+    train_losses = []
+    print(len(train_dataset))
+    for seq_true in train_dataset:
+      optimizer.zero_grad()
+
+      seq_true = seq_true.to(device)
+      seq_pred = model(seq_true)
+
+      loss = criterion(seq_pred, seq_true)
+
+      loss.backward()
+      optimizer.step()
+
+      train_losses.append(loss.item())
+
+    val_losses = []
+    model = model.eval()
+    with torch.no_grad():
+      for seq_true in val_dataset:
+
+        seq_true = seq_true.to(device)
+        seq_pred = model(seq_true)
+
+        loss = criterion(seq_pred, seq_true)
+        val_losses.append(loss.item())
+
+    train_loss = np.mean(train_losses)
+    val_loss = np.mean(val_losses)
+
+    history['train'].append(train_loss)
+    history['val'].append(val_loss)
+
+    if val_loss < best_loss:
+      best_loss = val_loss
+      best_model_wts = copy.deepcopy(model.state_dict())
+
+    print(f'Epoch {epoch}: train loss {train_loss} val loss {val_loss}')
+    
+  model.load_state_dict(best_model_wts)
+  return model.eval(), history
+
+
+print("train model")
+model, history = train_model(
+  model, 
+  train_dataset, 
+  val_dataset, 
+  n_epochs=15
+)
+
+ax = plt.figure().gca()
+
+ax.plot(history['train'])
+ax.plot(history['val'])
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['train', 'test'])
+plt.title('Loss over training epochs')
 plt.show()
