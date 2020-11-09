cda9e8d709909fb31b9aa60b06185bfd33ae1df4
osta0005 <olinestaerke@hotmail.com>
2020-11-09 13:23:32 +0100

    Preprocessing - remove missing valye

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import io
+import json
+import os
+
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
 
 cwd = os.getcwd()
 df = pd.read_csv(cwd+'/data/sensor1.csv')
 print(df.head())
+
+df_sensor_1 = df[df['sensor_id'] == 1]
+_ = df_sensor_1.set_index('timestamp')[['temperature', 'humidity', 'ohms', 'moisture']].plot(subplots=True, figsize=(23, 14))
+plt.show()
