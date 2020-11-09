7f9fe28f4fb6feb898e802050a0d33460f16b9bb
Natasha Klingenbrunn <natashaklingenbrunn@mbptilhenatasha.clients.wireless.dtu.dk>
2020-11-09 16:40:57 +0100

    Creating DataLoader

--- data_analysis.py
+++ data_analysis.py
@@ -6,7 +6,7 @@ import seaborn as sn
 df = pd.read_csv(os.getcwd()+'/Data/sensor1.csv')
 
 # Understanding Data
-print(df.tail())
+# print(df.tail())
 
 # Humidity at sensor
 # ax = df.humidity.plot.line()
@@ -25,9 +25,9 @@ print(df.tail())
 # print(df.timestamp.max())
 
 # Correlation Matrix
-corrMatrix = df[df['sensor_id'] == 4].corr()
-sn.heatmap(corrMatrix)
-plt.show()
+# corrMatrix = df[df['sensor_id'] == 4].corr()
+# sn.heatmap(corrMatrix)
+# plt.show()
 
 # Plotting
 # ax.plot()
@@ -35,3 +35,15 @@ plt.show()
 # ax.set_ylabel("Frequency")
 # ax.set_title("Wind Direction")
 # plt.show()
+
+# Plotting BY SENSOR
+# df = pd.read_csv(os.getcwd()+'/Data/sensor1.csv')
+# fig, axs = plt.subplots(6,7)
+# fig.set_size_inches(15, 15)
+# for (i, plots) in enumerate(axs.flatten()):
+#     df_s1_m = df[df["sensor_id"] == i+1]["humidity"]
+#     plots.hist(df_s1_m, bins = 30, range=(0,100))
+
+
+# fig.tight_layout()
+# fig.show()
--- main.py
+++ main.py
@@ -5,6 +5,77 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sn
+from torch.utils import data
 
-# Importing data
-df = pd.read_csv(os.getcwd()+'/Data/sensor1.csv')
+
+class Dataset(data.Dataset):
+    def __init__(self, inputs, targets):
+        self.inputs = inputs
+        self.targets = targets
+
+    def __len__(self):
+        # Return the size of the dataset
+        return len(self.targets)
+
+    def __getitem__(self, index):
+        # Retrieve inputs and targets at the given index
+        X = self.inputs[index]
+        y = self.targets[index]
+
+        return X, y
+
+    
+def create_datasets(sequences, dataset_class, p_train=0.8, p_val=0.1, p_test=0.1):
+
+    # number of sensors
+    num_sensors = len(set(sequences.sensor_id))
+
+    # shuffle the data by sensor
+    sequences = sequences.sample(frac=1.0)
+
+    # Define partition sizes
+    num_train = int(len(sequences)*p_train)
+    num_val = int(len(sequences)*p_val)
+    num_test = int(len(sequences)*p_test)
+
+    # Split sequences into partitions
+    sequences_train = sequences[:num_train]
+    sequences_val = sequences[num_train:num_train+num_val]
+    sequences_test = sequences[-num_test:]
+
+    def get_inputs_targets_from_sequences(sequences):
+        # Define empty lists
+        inputs, targets = [], []
+        
+        # Append inputs and targets s.t. both lists contain L-1 words of a sentence of length L
+        # but targets are shifted right by one so that we can predict the next word
+        for sequence in sequences:
+            inputs.append(sequence[:-1])
+            targets.append(sequence[1:])
+            
+        return inputs, targets
+
+    # Get inputs and targets for each partition
+    inputs_train, targets_train = get_inputs_targets_from_sequences(sequences_train)
+    inputs_val, targets_val = get_inputs_targets_from_sequences(sequences_val)
+    inputs_test, targets_test = get_inputs_targets_from_sequences(sequences_test)
+
+    # Create datasets
+    training_set = dataset_class(inputs_train, targets_train)
+    validation_set = dataset_class(inputs_val, targets_val)
+    test_set = dataset_class(inputs_test, targets_test)
+
+    return training_set, validation_set, test_set
+    
+# # Importing data
+sequences = pd.read_csv(os.getcwd()+'/Data/sensor1.csv')
+# sequences = sequences['moisture']
+
+# training_set, validation_set, test_set = create_datasets(sequences, Dataset)
+
+
+print(len(set(sequences.sensor_id)))
+
+# for each sensor
+# make a list of its moisture values
+# 
