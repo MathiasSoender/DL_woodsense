90b3bdf4889edf87d7610d030c53a6565f70db94
Natasha Klingenbrunn <natashaklingenbrunn@mbptilhenatasha.clients.wireless.dtu.dk>
2020-11-16 14:17:17 +0100

    dataloader working

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import io
-import json
-import os
 
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sn
-from torch.utils import data
+import os
+import random # to shuffle
+
+import matplotlib.pyplot as plt # plotting
+import pandas as pd # plotting
+import seaborn as sn # plotting
+from torch.utils import data # for the dataloader
 
 
 class Dataset(data.Dataset):
@@ -24,34 +24,40 @@ class Dataset(data.Dataset):
 
         return X, y
 
-    
+
 def create_datasets(sequences, dataset_class, p_train=0.8, p_val=0.1, p_test=0.1):
 
-    # number of sensors
-    num_sensors = len(set(sequences.sensor_id))
+    # sensors
+    sensors = [i for i in range(len(set(sequences.sensor_id)))]
 
     # shuffle the data by sensor
-    sequences = sequences.sample(frac=1.0)
+    random.shuffle(sensors)
 
     # Define partition sizes
-    num_train = int(len(sequences)*p_train)
-    num_val = int(len(sequences)*p_val)
-    num_test = int(len(sequences)*p_test)
+    num_train = int(len(sensors)*p_train)
+    num_val = int(len(sensors)*p_val)
+    num_test = int(len(sensors)*p_test)
+    
 
-    # Split sequences into partitions
-    sequences_train = sequences[:num_train]
-    sequences_val = sequences[num_train:num_train+num_val]
-    sequences_test = sequences[-num_test:]
+    # Split sensor indices into partitions
+    sensors_train = sensors[:num_train]
+    sensors_val = sensors[num_train:num_train+num_val]
+    sensors_test = sensors[-num_test:]
+
+    # capture dataframes for the indiced sensors
+    sequences_train  = sequences[sequences['sensor_id'].isin(sensors_train)]
+    sequences_val  = sequences[sequences['sensor_id'].isin(sensors_val)]
+    sequences_test  = sequences[sequences['sensor_id'].isin(sensors_test)]
 
     def get_inputs_targets_from_sequences(sequences):
         # Define empty lists
+
         inputs, targets = [], []
         
-        # Append inputs and targets s.t. both lists contain L-1 words of a sentence of length L
-        # but targets are shifted right by one so that we can predict the next word
-        for sequence in sequences:
-            inputs.append(sequence[:-1])
-            targets.append(sequence[1:])
+        # for each sensor
+        for (sensor, sequence) in sequences.groupby('sensor_id'):
+            inputs.append(sequence['moisture'][:-1].tolist())
+            targets.append(sequence['moisture'][1:].tolist())
             
         return inputs, targets
 
@@ -60,22 +66,18 @@ def create_datasets(sequences, dataset_class, p_train=0.8, p_val=0.1, p_test=0.1
     inputs_val, targets_val = get_inputs_targets_from_sequences(sequences_val)
     inputs_test, targets_test = get_inputs_targets_from_sequences(sequences_test)
 
-    # Create datasets
+    # # Create datasets
     training_set = dataset_class(inputs_train, targets_train)
     validation_set = dataset_class(inputs_val, targets_val)
     test_set = dataset_class(inputs_test, targets_test)
 
     return training_set, validation_set, test_set
     
-# # Importing data
+# Importing data
 sequences = pd.read_csv(os.getcwd()+'/Data/sensor1.csv')
-# sequences = sequences['moisture']
 
-# training_set, validation_set, test_set = create_datasets(sequences, Dataset)
+# capturing the relevant columns
+sequences = sequences[['sensor_id','moisture']]
 
-
-print(len(set(sequences.sensor_id)))
-
-# for each sensor
-# make a list of its moisture values
-# 
+# creating the datasets
+training_set, validation_set, test_set = create_datasets(sequences, Dataset)
